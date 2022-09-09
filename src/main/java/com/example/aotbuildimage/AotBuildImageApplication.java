79397232f8e52d521ce5f49202447230ea506166
Scott Frederick <sfrederick@vmware.com>
2022-09-09 09:31:13 -0500

    Initial commit

---
+++ src/main/java/com/example/aotbuildimage/AotBuildImageApplication.java
@@ -0,0 +1,13 @@
+package com.example.aotbuildimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AotBuildImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AotBuildImageApplication.class, args);
+	}
+
+}
